28f8ca8dc33d1e3640ad5469a115e9e2d87a0af6
roshan-io <sheik.roshan.edu@gmail.com>
2026-03-08 16:30:22 +0530

    Integrate Firebase and add data writing function

--- web/app.js
+++ web/app.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "TEST",
+  authDomain: "TEST",
+  projectId: "TEST",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.writeData = async function () {
+  await setDoc(doc(db, "test", "hello"), {
+    message: "Hello from APK"
+  });
+  alert("Data written");
+};
