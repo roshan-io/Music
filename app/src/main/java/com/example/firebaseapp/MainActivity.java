3ee6ae63f83479094da9d63552a966302f31056f
roshan-io <sheik.roshan.edu@gmail.com>
2026-03-08 16:31:09 +0530

    Implement WebView to load local HTML file

--- app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -1,1 +1,24 @@
+package com.example.firebaseapp;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        web = new WebView(this);
+        setContentView(web);
+
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+
+        web.loadUrl("file:///android_asset/index.html");
+    }
+}
